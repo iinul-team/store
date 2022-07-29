320bf7314d5a8ba1649b600385b10dc20d32754d
olup <loup.topalian@gmail.com>
2022-07-29 21:19:24 +0200

    fix: object selection

--- scripts/generate-store.mjs
+++ scripts/generate-store.mjs
@@ -1,5 +1,6 @@
 #!/usr/bin/env zx
-
+console.log(process.env.AIRTABLE_API_KEY);
+// Get the store packs from airtable
 let resp = await fetch(
   "https://api.airtable.com/v0/appPeKJE2w7yfGZyJ/Nos%20Histoires",
   {
@@ -9,5 +10,11 @@ let resp = await fetch(
   }
 ).then((r) => r.json());
 
-const currated = resp.filter((e) => !!e.is_confirmed);
+console.log(resp);
+// filters only the one that have been confirmed
+const currated = resp.records.filter((e) => !!e.is_confirmed);
+
+// publish json
+await fs.remove("./build");
+await fs.ensureDir("./build");
 await fs.writeJSON("build/store.json", currated);
